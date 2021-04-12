b9b087857f3271a701b71e1d409ae036849949ba
Juan Jose Vargas Fletes <androidapps0703@gmail.com>
2021-04-12 12:29:33 -0600

    Testing

--- main.go
+++ main.go
@@ -57,36 +57,62 @@ func HandlerUsers(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	records := make(map[string]plotter.XYs)
+	recordsTime := make(map[string]plotter.XYs)
 
 	for i := 1; i <= 20; i++ { // Restricciones
 		// Hacemos una media para que sea mas correcto
 		var promIteracionesfb float64 = 0
+		var promTiempofb float64 = 0
 		var promIteraciones float64 = 0
+		var promTiempo float64 = 0
 
-		for j := 0; j < 100; j++ { // Hacemos una media
-			rest := GenerateRest(i, GetCategorias())
-			solucion := GetSolution(GetCategorias(), rest)
+		rest := GenerateRest(i, GetCategorias())
+		solucion := GetSolution(GetCategorias(), rest)
+
+		for j := 0; j < 10; j++ { // Hacemos una media
 			encontrada := make([]Categorias, 0)
 			mensajes := make([]sockets.Message, 0)
 			eliminadas := make(map[string]bool)
 
 			// Medimos el tiempo
-			//initTime := GetCurrentTime()
+			initTime := GetCurrentTime()
 
 			iteracionesfb, _ := FuerzaBruta(GetCategorias(), solucion, encontrada, &mensajes, rest)
 
+			time := GetCurrentTime() - initTime
+
+			promTiempofb += float64(time)
+
+			encontrada = make([]Categorias, 0)
+			mensajes = make([]sockets.Message, 0)
+			eliminadas = make(map[string]bool)
+
+			initTime = GetCurrentTime()
+
 			iteraciones, _ := Backtracking(GetCategorias(), solucion, encontrada, rest, &eliminadas, &mensajes)
 
-			//time := GetCurrentTime() - initTime
+			time = GetCurrentTime() - initTime
+
+			promTiempo += float64(time)
 
 			promIteraciones += float64(iteraciones)
 			promIteracionesfb += float64(iteracionesfb)
-			//promTiempo += float64(time)
 		}
 
 		promIteraciones /= 100
 		promIteracionesfb /= 100
-		//promTiempo /= 100
+		promTiempo /= 100
+		promTiempofb /= 100
+
+		recordsTime["Backtracking"] = append(records["Backtracking"], plotter.XY{
+			X: float64(i),
+			Y: promTiempo,
+		})
+
+		recordsTime["FuerzaBruta"] = append(records["FuerzaBruta"], plotter.XY{
+			X: float64(i),
+			Y: promTiempofb,
+		})
 
 		records["Backtracking"] = append(records["Backtracking"], plotter.XY{
 			X: float64(i),
@@ -100,6 +126,7 @@ func main() {
 	}
 
 	p, err := plot.New()
+	ptime, err := plot.New()
 
 	if err != nil {
 		return
@@ -109,7 +136,15 @@ func main() {
 	p.X.Label.Text = "Restricciones"
 	p.Y.Label.Text = "Iteraciones"
 
+	ptime.Title.Text = "Rendimiento segun restricciones"
+	ptime.X.Label.Text = "Restricciones"
+	ptime.Y.Label.Text = "Tiempo"
+
 	err = plotutil.AddLinePoints(p,
+		"Backtracking", records["Backtracking"],
+		"FuerzaBruta", records["FuerzaBruta"])
+
+	err = plotutil.AddLinePoints(ptime,
 		"Backtracking", records["Backtracking"],
 		"FuerzaBruta", records["FuerzaBruta"])
 
@@ -117,7 +152,10 @@ func main() {
 		return
 	}
 
-	if err := p.Save(7*vg.Inch, 7*vg.Inch, "points.png"); err != nil {
+	if err := p.Save(7*vg.Inch, 7*vg.Inch, "iteraciones.png"); err != nil {
+		panic(err)
+	}
+	if err := ptime.Save(7*vg.Inch, 7*vg.Inch, "tiempo.png"); err != nil {
 		panic(err)
 	}
 }
